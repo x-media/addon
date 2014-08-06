2eec4a61766bea88662573f4d7e40d8c69c569d7
x-media <x.mediagit@outlook.com>
2014-08-06 22:02:15 +0200

    parameter & api

--- addon/engine/func/func_API.lua
+++ addon/engine/func/func_API.lua
@@ -1,1 +1,28 @@
 local x, c, l = select(2, ...):unpack()
+
+-- locals
+local blank = c["media"]["texture"]["blank"]
+local normal = c["media"]["texture"]["normal"]
+
+-- merge api
+local function xAPI(object)
+	local mt = getmetatable(object).__index
+
+	--if not object.Size then mt.Size = Size end
+end
+
+local handled = {["Frame"] = true}
+local object = CreateFrame("Frame")
+xAPI(object)
+xAPI(object:CreateTexture())
+xAPI(object:CreateFontString())
+
+object = EnumerateFrames()
+while object do
+	if not handled[object:GetObjectType()] then
+		xAPI(object)
+		handled[object:GetObjectType()] = true
+	end
+
+	object = EnumerateFrames(object)
+end
--- addon/engine/func/func_Core.lua
+++ addon/engine/func/func_Core.lua
@@ -1,3 +1,17 @@
 local x, c, l = select(2, ...):unpack()
 
--- parameter
+-- character parameter
+x.Class = select(2, UnitClass("player"))
+x.Name = select(1, UnitName("player"))
+x.Level = UnitLevel("player")
+x.Faction = UnitFactionGroup("player")
+x.Realm = GetRealmName()
+
+-- client parameter
+x.Locale = GetLocale()
+x.Patch, x.Build, x.Release, x.Toc = GetBuildInfo()
+
+-- addon parameter
+x.Version = GetAddOnMetadata("addon", "Version")
+x.vNumber = tonumber(x.Version)
+x.Dump = function() return end
